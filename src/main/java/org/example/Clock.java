9303e59c688f77d33fa11c0c9639405b4f2a8af2
Cameron Fung <Cameronfuung@gmail.com>
2023-02-28 16:36:51 -0800

    pushing

--- src/main/java/org/example/Clock.java
+++ src/main/java/org/example/Clock.java
@@ -2,13 +2,32 @@ package org.example;
 import java.time.Duration;
 import java.time.Instant;
 
+import static java.lang.Math.abs;
+
 public class Clock {
 
     Instant prev;
 
     Instant current;
 
+    double tickTime;
+
+    public Clock() {
+      prev = Instant.now();
+      tickTime = 500;
+    }
+
   public boolean tick() {
-    return true;
+
+    current = Instant.now();
+
+    Duration timeElapsed = Duration.between(prev, current);
+    System.out.println(abs(timeElapsed.toMillis()));
+
+    if (timeElapsed.toMillis() > tickTime) {
+      prev = Instant.now();
+      return true;
+    }
+    return false;
   }
 }
