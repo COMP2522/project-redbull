35a4426ec82a945f4b5e0741eb4e84c61f1aba62
Adam Shergill <shergilladam04@gmail.com>
2023-03-13 19:54:43 -0700

    Created the tileTest class which tests the Tile constructor and isWall boolean value from the Tile class.

--- src/main/java/org/example/Tile.java
+++ src/main/java/org/example/Tile.java
@@ -5,7 +5,7 @@ import java.awt.*;
 //Tile class which represents the movable tiles and is a Sprite
 public class Tile extends Sprite{
     Image image;
-    boolean isWall;
+    public boolean isWall;
     //Public tile constructor taking in x, y ,size, picture, and isWall
     public Tile(int xPos, int yPos, int size, Image picture, boolean isWall) {
         super(xPos, yPos, size, picture);
---
+++ src/main/java/test/tileTest.java
@@ -0,0 +1,89 @@
+package test;
+
+import org.example.Tile;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The purpose of the tileTest class is to test the Tile constructor and ensure the boolean isWall are working
+ * correctly.
+ */
+public class tileTest {
+
+  @Test
+  public void testConstructor() {
+    Image image = new Image() {
+      @Override
+      public int getWidth(ImageObserver imageObserver) {
+        return 0;
+      }
+
+      @Override
+      public int getHeight(ImageObserver imageObserver) {
+        return 0;
+      }
+
+      @Override
+      public ImageProducer getSource() {
+        return null;
+      }
+
+      @Override
+      public Graphics getGraphics() {
+        return null;
+      }
+
+      @Override
+      public Object getProperty(String s, ImageObserver imageObserver) {
+        return null;
+      }
+    };
+    Tile tile = new Tile(0, 0, 10, image, true);
+    assertEquals(0, tile.getxPos());
+    assertEquals(0, tile.getyPos());
+    assertEquals(10, tile.getSize());
+    assertEquals(image, tile.getPicture());
+    assertTrue(tile.isWall);
+  }
+
+  @Test
+  public void testIsWall() {
+    Image image = new Image() {
+      @Override
+      public int getWidth(ImageObserver imageObserver) {
+        return 0;
+      }
+
+      @Override
+      public int getHeight(ImageObserver imageObserver) {
+        return 0;
+      }
+
+      @Override
+      public ImageProducer getSource() {
+        return null;
+      }
+
+      @Override
+      public Graphics getGraphics() {
+        return null;
+      }
+
+      @Override
+      public Object getProperty(String s, ImageObserver imageObserver) {
+        return null;
+      }
+    };
+    Tile tile1 = new Tile(0, 0, 10, image, true);
+    assertTrue(tile1.isWall);
+
+    Tile tile2 = new Tile(0, 0, 10, image, false);
+    assertFalse(tile2.isWall);
+  }
+
+}
