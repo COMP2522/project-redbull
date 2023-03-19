76b15875b9928aaaf0c132abc8f3c0d951570cbf
adams <shergilladam04@gmail.com>
2023-03-19 01:16:53 -0700

    Created the clockTest class which tests out the functionality of the clock class. Also commented the test class for easy understanding.

---
+++ src/main/java/test/clockTest.java
@@ -0,0 +1,62 @@
+package test;
+import org.example.Clock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * The purpose of the clockTest class is to test the functionality of the Clock class.
+ */
+class clockTest {
+
+    // Creating a private clock for the clockTest class.
+    private Clock clock;
+
+
+    // Creates a new Clock before all new tests
+    @BeforeEach
+    void setUp() {
+        clock = new Clock();
+    }
+
+
+
+    // Tests out the functionality of the clocks reset ability.
+    @Test
+    void testReset() {
+
+
+        clock.reset();
+
+        assertFalse(clock.tick());
+    }
+
+
+    // Ensures that the clock succesfully gets the frames per second
+    @Test
+    void testGetFramesPerSecond() {
+
+
+        float fps = Clock.getFramesPerSecond();
+
+        // Frames per second should be greater than 0
+        assertTrue(fps > 0);
+    }
+
+
+    // Ensures that the clock gets the frames per clock
+    @Test
+    void testGetFramesPerClock() {
+
+
+        float fpc = Clock.getFramesPerClock();
+
+        // Frames per clock should be greater than 0
+        assertTrue(fpc > 0);
+    }
+
+}
