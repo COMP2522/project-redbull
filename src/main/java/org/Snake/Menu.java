83e3634940ffb110b0e4b5e9bd94564ef791f52a
Cameron Fung <Cameronfuung@gmail.com>
2023-03-28 08:42:19 -0700

    changed background

---
+++ src/main/java/org/Snake/Menu.java
@@ -0,0 +1,19 @@
+package org.Snake;
+
+import processing.core.PApplet;
+
+public class Menu extends UIComponent {
+  public Menu(PApplet parent, float x, float y, float width, float height) {
+    super(parent, x, y, width, height);
+  }
+
+  @Override
+  public void draw() {
+
+  }
+
+  @Override
+  public void mouseClicked(float mx, float my) {
+
+  }
+}
