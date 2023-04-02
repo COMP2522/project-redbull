6318423537a972aa5f7db2519c717098dede9d7f
Cameron Fung <Cameronfuung@gmail.com>
2023-04-01 18:36:14 -0700

    made classes that will make up the menu

---
+++ src/main/java/org/Snake/Button.java
@@ -0,0 +1,45 @@
+package org.Snake;
+
+import processing.core.PApplet;
+
+public class Button extends UIComponent {
+
+  private String label;
+  private boolean pressed;
+
+  public Button(PApplet parent, float x, float y, float width, float height, String label) {
+    super(parent, x, y, width, height);
+    this.label = label;
+    this.pressed = false;
+  }
+
+  @Override
+  public void draw() {
+    parent.stroke(0);
+    if (pressed) {
+      parent.fill(200, 0, 0);
+    } else {
+      parent.fill(255);
+    }
+    parent.rect(x, y, width, height);
+    parent.fill(0);
+    parent.text(label, x + width/2, y + height/2);
+  }
+
+  @Override
+  public void mouseClicked(float mx, float my) {
+    if (contains(mx, my)) {
+      pressed = true;
+    } else {
+      pressed = false;
+    }
+  }
+
+  public boolean isPressed() {
+    return pressed;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
+}
