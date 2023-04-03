867cd52563dfb563435168844c1195f1dd8947f6
adams <shergilladam04@gmail.com>
2023-04-02 18:36:15 -0700

    Comepleted the WindowTest class that tests the Window class.

--- src/main/java/test/WindowTest.java
+++ src/main/java/test/WindowTest.java
@@ -1,59 +1,57 @@
-//package test;
-//
-//import org.example.Window;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class WindowTest {
-//
-//  @Test
-//  public void testGetWidth() {
-//    // Arrange
-//    Window window = new Window();
-//
-//    // Act
-//    int width = window.getWidth();
-//
-//    // Assert
-//    assertEquals(1080, width);
-//  }
-//
-//  @Test
-//  public void testGetHeight() {
-//    // Arrange
-//    Window window = new Window();
-//
-//    // Act
-//    int height = window.getHeight();
-//
-//    // Assert
-//    assertEquals(1080, height);
-//  }
-//
-//  @Test
-//  public void testGetOffset() {
-//    // Arrange
-//    Window window = new Window();
-//
-//    // Act
-//    int offset = window.getOffset();
-//
-//    // Assert
-//    assertEquals(0, offset);
-//  }
-//
-//  @Test
-//  public void testReset() {
-//    // Arrange
-//    Window window = new Window();
-//
-//    // Act
-//    window.reset();
-//
-//    // Assert
-//    assertEquals(0, window.lastKeyPressed);
-//    assertEquals(0, window.spriteManager.getSnake().getBody().size());
-//    assertFalse(window.clock.isPaused());
-//  }
-//}
+package test;
+
+
+import org.Snake.Window;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static processing.core.PApplet.min;
+
+/**
+ * JUnit 5 test class for the Window class
+ */
+@DisplayName("Window class tests")
+class WindowTest {
+
+    // Initializes window.
+    private Window window;
+
+    //Gets the screen size of the user.
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+    // Sets up a new window before each test.
+    @BeforeEach
+    void setUp() {
+        window = new Window();
+    }
+
+    // Tests if window gets the correct width depending on the users monitor screen size.
+    @Test
+    @DisplayName("Test getWidth method")
+    void testGetWidth() {
+
+        int expected = min((int) (screenSize.getWidth()*0.99), (int) (screenSize.getHeight()*0.99));
+        int actual = window.getWidth();
+        assertEquals(expected, actual);
+    }
+
+
+    // Gets the Offset depending on the users screen size.
+    @Test
+    @DisplayName("Test getOffset method")
+    void testGetOffset() {
+
+        int width = min((int) (screenSize.getWidth() * 0.99), (int) (screenSize.getHeight() * 0.99));
+        int expected = (int) ((screenSize.getWidth()-width)/(2));
+        int actual = window.getOffset();
+        assertEquals(expected, actual);
+    }
+
+
+
+}
