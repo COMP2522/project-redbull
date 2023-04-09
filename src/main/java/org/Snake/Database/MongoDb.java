85caf07510764fbaff4468456919edbefa361da9
adams <shergilladam04@gmail.com>
2023-04-09 00:04:10 -0700

    Commented out the KVPair program and the MongoDb program for much better readability and understanding.

--- src/main/java/org/Snake/Database/MongoDb.java
+++ src/main/java/org/Snake/Database/MongoDb.java
@@ -1,5 +1,6 @@
 package org.Snake.Database;
 
+// Imports
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerApi;
@@ -15,9 +16,21 @@ import java.util.TreeMap;
 
 import static com.mongodb.client.model.Filters.eq;
 
+/**
+ * The MongoDb class communicates with our database.
+ *
+ * @author Cameron Fung
+ */
 public class MongoDb {
+
+  //Instantiates the database variable.
   MongoDatabase database;
 
+  /**
+   * The MongoDb method is what is actually doing the connecting to the database.
+   *
+   * This ensures we can connect to our MongoDb database.
+   */
   public MongoDb() {
     ConnectionString connectionString = new ConnectionString("mongodb+srv://MasterUser:AuOhjOhAmZbmiqG1@cluster0.cre2ejv.mongodb.net/?retryWrites=true&w=majority");
     MongoClientSettings settings = MongoClientSettings.builder()
@@ -34,6 +47,14 @@ public class MongoDb {
       System.out.println("Collection already in database.");
     }
   }
+
+  /**
+   * This put method puts data into our database.
+   *
+   * @param name - The name of the user.
+   * @param playerScore - The score the player achieved.
+   * @param level - The level which the user was playing.
+   */
   public void put(String name, int playerScore, String level) {
     ArrayList<KVPair> scores = get(level);
     if (scores.size() >= 10) {
@@ -50,8 +71,14 @@ public class MongoDb {
       doc.append("score", playerScore);
       database.getCollection(level).insertOne(doc);
     }
-  };
+  }
 
+  /**
+   * Getter to retrieve data from the database.
+   *
+   * @param levelName - The level name we want the data for
+   * @return - Returns the scores of all users who have attempted the level.
+   */
   public ArrayList<KVPair> get(String levelName) {
     FindIterable<Document> res = database.getCollection(levelName).find();
 
@@ -65,6 +92,13 @@ public class MongoDb {
     return scores;
   }
 
+  /**
+   * Checks if a user has obtained a high score after they have completed a snake game.
+   *
+   * @param score - The score the player obtained.
+   * @param level - The level the user was playing.
+   * @return Returns true if the player obtained the high score.
+   */
   public boolean isHighScore(int score, String level) {
     ArrayList<KVPair> scores = get(level);
     if (scores.size() < 10) {
@@ -74,6 +108,11 @@ public class MongoDb {
     }
   }
 
+  /**
+   * Main method that drives the MongoDb class.
+   *
+   * @param args - Unused.
+   */
   public static void main(String[] args) {
     MongoDb db = new MongoDb();
 
